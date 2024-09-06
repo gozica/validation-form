ab4306a6ca8470e5e6ae7e97ccd0dba95fb03fbc
gozica <109421350+gozica@users.noreply.github.com>
2024-09-06 16:28:04 -0400

    main js

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+const form = document.getElementById('mainForm');
+const name = document.getElementById("name");
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
+const password = document.getElementById("password");
+const message = document.getElementById("message");
+
+
+
+})
