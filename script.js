fc5a258266fedc44172b18de15b46384a2b304db
gozica <109421350+gozica@users.noreply.github.com>
2024-09-06 20:15:04 -0400

    eventlisteners

--- script.js
+++ script.js
@@ -6,6 +6,47 @@ const phone = document.getElementById("phone");
 const password = document.getElementById("password");
 const message = document.getElementById("message");
 
+//eventlisteners
+
+
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    if (checkInputs()) {
+      showModal();
+    }
+  });
+
+  name.addEventListener("input", () => {
+    validateField(name, name.value.trim() !== "", "Name cannot be blank");
+  });
+
+  email.addEventListener("input", () => {
+    validateField(email, isEmail(email.value.trim()), "Not a valid email");
+  });
+
+  phone.addEventListener("input", () => {
+    validateField(
+      phone,
+      isPhone(phone.value.trim()),
+      "Not a valid phone number"
+    );
+  });
+
+  password.addEventListener("input", () => {
+    validateField(
+      password,
+      password.value.trim().length >= 8,
+      "Password must be 8 characters or longer"
+    );
+  });
+
+  message.addEventListener("input", () => {
+    validateField(
+      message,
+      message.value.trim() !== "",
+      "Message cannot be blank"
+    );
+  });
 
 
 })
